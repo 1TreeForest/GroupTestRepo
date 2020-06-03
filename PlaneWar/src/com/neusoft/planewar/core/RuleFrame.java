69218c6787f90aa9e2d21d430302d7f2feb1b711
Tricia-07 <62382682+Tricia-07@users.noreply.github.com>
2020-06-03 22:31:03 +0800

    Update RuleFrame.java

--- PlaneWar/src/com/neusoft/planewar/core/RuleFrame.java
+++ PlaneWar/src/com/neusoft/planewar/core/RuleFrame.java
@@ -11,12 +11,13 @@ public class RuleFrame extends JFrame {
 
 	public RuleFrame() {
 		super();
-		rule=new ImageIcon("com/neusoft/planewar/img/rule.png");
+		rule=new ImageIcon("src/com/neusoft/planewar/img/rule.png");
 		JLabel imgLabel = new JLabel(rule);// 将背景图放在标签里。
-		imgLabel.setBounds(0, 0, rule.getIconWidth(), rule.getIconHeight());// 设置背景标签的位置	
+		imgLabel.setBounds(0, 0, 224, 420);// 设置背景标签的位置	
 		rulePanel = (JPanel) this.getContentPane();// 将Frame的内容窗格放到Panel上
 		rulePanel.setOpaque(false);// 将内容面板设为透明。这样LayeredPane面板中的背景才能显示出来。
 		rulePanel.setLayout(null); // 不设置布局管理器，不需要
+		rulePanel.add(imgLabel,-1);
 		this.getLayeredPane().add(imgLabel, new Integer(Integer.MIN_VALUE));// 将背景标签添加到JFrame的LayeredPane面板里。
 		this.setResizable(false);		
 	}
