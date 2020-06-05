80a1e8804957ccef194dc9837822fb77837d082d
Tricia-07 <62382682+Tricia-07@users.noreply.github.com>
2020-06-05 15:35:59 +0800

    Update EnemyPlane.java

--- PlaneWar/src/com/neusoft/planewar/core/EnemyPlane.java
+++ PlaneWar/src/com/neusoft/planewar/core/EnemyPlane.java
@@ -117,18 +117,14 @@ public class EnemyPlane extends Plane {
 				missile.y += height;
 				pwc.missiles.add(missile);
 			} else if (m == 1) {
-				Missile missile = new Missile(pwc, this.x, this.y, "enemyPlane_missile_0" + type, type, good);
-				int theta = 45 / 360;
-				missile.x = (int) (missile.x + (width / 2 + r * Math.sin(Math.toRadians(theta)) - missile.width / 2));
-				missile.y = (int) (missile.y
-						- ((r * Math.cos(Math.toRadians(theta)) - height / 2 + missile.height / 2)));
+				Missile missile = new Missile(pwc, this.x, this.y, "enemyPlane_missile_0" + type, 11, good);
+				missile.x += (this.width - missile.width) / 2;
+				missile.y += height;
 				pwc.missiles.add(missile);
 			} else if (m == 2) {
-				Missile missile = new Missile(pwc, this.x, this.y, "enemyPlane_missile_0" + type, type, good);
-				int theta = 45 / 360;
-				missile.x = (int) (missile.x - (width / 2 + r * Math.sin(Math.toRadians(theta)) - missile.width / 2));
-				missile.y = (int) (missile.y
-						- ((r * Math.cos(Math.toRadians(theta)) - height / 2 + missile.height / 2)));
+				Missile missile = new Missile(pwc, this.x, this.y, "enemyPlane_missile_0" + type, 12, good);
+				missile.x += (this.width - missile.width) / 2;
+				missile.y += height;
 				pwc.missiles.add(missile);
 			}
 		}
